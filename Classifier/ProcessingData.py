bd73a2590ffc0f5bc926000cf17ddfdaf8f585b4
iTing <35248361+FatTig3R@users.noreply.github.com>
2019-04-12 11:51:58 +0800

    Add files via upload

---
+++ Classifier/ProcessingData.py
@@ -0,0 +1,96 @@
+import os
+from PIL import Image
+import numpy as np
+import random
+import shutil
+
+class ProcessingData(object):
+    """
+    定义一个处理数据的类
+    功能：
+    1, rename the images
+    2, resize the images
+    3, images type conversion
+    4, create folder
+    """
+
+    def __init__(self, Raw_Images_Folder, Train_Folder, Test_Folder, Type):
+
+        self.raw_images_folder = Raw_Images_Folder
+        self.train_folder = Train_Folder
+        self.test_folder = Test_Folder
+        self.type = Type
+
+        # 创建文件夹
+        if not os.path.exists(self.raw_images_folder):
+            os.mkdir(self.raw_images_folder)
+        if not os.path.exists(self.train_folder):
+            os.mkdir(self.train_folder)
+        if not os.path.exists(self.test_folder):
+            os.mkdir(self.test_folder)
+
+    def renameImages(self, filePath, Type):
+        """
+        定义一个图像重命名的方法
+        用于对图片的重命名
+        方便提取标签
+        :param filePath: 图片存在的一级目录
+        :param Type:
+        :return:
+        """
+        folder_counter = 0
+        for eve_type_folder in Type:
+            file_counter = 0
+            folders = os.listdir(filePath + eve_type_folder)
+            for eve_type_image in folders:
+                file_counter += 1
+                os.rename(src=filePath + eve_type_folder + '/' + eve_type_image,
+                          dst=filePath + eve_type_folder + '/' + str(folder_counter) + '_' +
+                              eve_type_image.split('.')[
+                                  0] + '.jpg')
+            folder_counter += 1
+
+    def resizeImages(self, filePath, Type=None, dstPath=None, Width=100, Height=100):
+        """
+        定义一个调整图片尺寸大小的方法
+
+        :param filePath:
+        :param Type:
+        :param dstPath:
+        :param Width:
+        :param Height:
+        :return:
+        """
+        for eve_type_folder in Type:
+            for eve_type_image in os.listdir(filePath + eve_type_folder):
+                image_open = Image.open(fp=filePath + eve_type_folder + '/' + eve_type_image)
+                try:
+                    new_image = image_open.resize((Width, Height), Image.BILINEAR)
+                    new_image.save(os.path.join(dstPath, os.path.basename(eve_type_image)))
+                except Exception as e:
+                    print(e)
+
+    def readImages(self, fileName, folder):
+        """
+        定义一个将图片转换为numpy的array类型的方法
+        :param fileName:
+        :param folder:
+        :return:  numpy array
+        """
+        images_open = Image.open(folder + fileName)
+        return np.array(images_open)
+
+    def moveImagesToTestFolder(self):
+            pathDir = os.listdir(self.train_folder)  # 取图片的原始路径
+            filenumber = len(pathDir)
+            rate = 0.2  # 自定义抽取图片的比例，比方说100张抽20张，那就是0.2
+            picknumber = int(filenumber * rate)  # 按照rate比例从文件夹中取一定数量图片
+            sample = random.sample(pathDir, picknumber)  # 随机选取picknumber数量的样本图片
+            for name in sample:
+                shutil.move(self.train_folder + name, self.test_folder + name)
+            return
+
+    def initialize(self):
+        self.renameImages(filePath=self.raw_images_folder, Type=self.type)
+        self.resizeImages(filePath=self.raw_images_folder, Type=self.type, dstPath=self.train_folder)
+        self.moveImagesToTestFolder()
---
+++ Classifier/Training.py
@@ -0,0 +1,113 @@
+import os
+import numpy as np
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Flatten
+from keras.optimizers import SGD
+from keras.layers import Conv2D, MaxPooling2D
+from ProcessingData import ProcessingData
+
+
+class Training(object):
+
+    def __init__(self, Train_Folder, Test_Folder, Batch_Size, Epochs):
+        self.train_folder = Train_Folder
+        self.test_folder = Test_Folder
+        self.batch_size = Batch_Size
+        self.epochs = Epochs
+
+    def train(self):
+        train_img_list = []  # x_train
+        train_label_list = []  # y_train
+
+        test_img_list = []  # x_test
+        test_lebal_list = []  # y_test
+
+        # 将训练集图片转换成数组
+        for file_1 in os.listdir(self.train_folder):
+            file_img_to_array = processing_data.readImages(fileName=file_1, folder=self.train_folder)
+            train_img_list.append(file_img_to_array)
+            train_label_list.append(int(file_1.split('_')[0]))
+
+        train_img_list = np.array(train_img_list)
+        train_label_list = np.array(train_label_list)
+
+        # 将测试集图片转换成数组
+        for file_2 in os.listdir(self.test_folder):
+            file_img_to_array = processing_data.readImages(fileName=file_2, folder=self.test_folder)
+            test_img_list.append(file_img_to_array)
+            test_lebal_list.append(int(file_2.split('_')[0]))
+
+        test_img_list = np.array(test_img_list)
+        test_lebal_list = np.array(test_lebal_list)
+
+        # 将标签转化格式
+        y_train = np_utils.to_categorical(y=train_label_list)
+        y_test = np_utils.to_categorical(y=test_lebal_list)
+
+        # 将特征点从0~255转换成0~1提高特征提取精度
+        x_train = train_img_list.astype('float32')
+        x_test = test_img_list.astype('float32')
+        x_train /= 255
+        x_test /= 255
+
+        # 搭建卷积神经网络
+        model = Sequential()
+        model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(100, 100, 3)))
+        model.add(Conv2D(32, (3, 3), activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+
+        model.add(Conv2D(64, (3, 3), activation='relu'))
+        model.add(Conv2D(64, (3, 3), activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+
+        model.add(Flatten())
+        model.add(Dense(256, activation='relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(4, activation='softmax'))
+
+        sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+        model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+        model.fit(
+            x_train,
+            y_train,
+            batch_size=self.batch_size,  # 一轮训练10次
+            epochs=self.epochs  # 一共训练32轮
+        )
+        model.save_weights('weights.h5', overwrite=True)
+        score = model.evaluate(x_test, y_test, batch_size=10)
+        print(score)
+
+
+if __name__ == '__main__':
+    raw_images_folder = 'Raw_Images_Folder/'
+    train_folder = 'Train_Images_Folder/'
+    test_folder = 'Test_Images_Folder/'
+    batch_size = 10
+    epochs = 32
+
+    catType = ['孟买猫', '布偶猫', '暹罗猫', '英国短毛猫']
+
+    # initialize
+    processing_data = ProcessingData(
+        Raw_Images_Folder=raw_images_folder,
+        Train_Folder=train_folder,
+        Test_Folder=test_folder,
+        Type=catType,
+    )
+
+    training = Training(
+        Train_Folder=train_folder,
+        Test_Folder=test_folder,
+        Batch_Size=batch_size,
+        Epochs=epochs,
+    )
+
+    # processing data
+    # processing_data.initialize()
+
+    # trianing
+    training.train()
